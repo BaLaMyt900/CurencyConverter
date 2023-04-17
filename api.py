3c557985d44a2afdec9707dfaaf1bd83d02a20ec
BaLaMyt900 <57708651+BaLaMyt900@users.noreply.github.com>
2023-04-17 11:19:26 +0400

    Final

--- api.py
+++ api.py
@@ -2,16 +2,14 @@ import requests
 import json
 
 
-D = {'Рубль': 'RUB', 'Доллар': 'USD', 'Евро': 'EUR', 'Юань': 'CNY'}
+D = {'Рубль': 'RUB', 'Доллар': 'USD', 'Евро': 'EUR', 'Юань': 'CNY'}  # словарь валют и их символов для запроса
+
 
 def convert(cur_from, cur_to, amount: int):
-    # url = f"https://api.apilayer.com/fixer/convert?to={D[cur_to]}&from={D[cur_from]}&amount={amount}"
     url = f'https://currate.ru/api/?get=rates&pairs={D[cur_from]}{D[cur_to]}&key=dad5895f3e8060395348a5310ca8f846'
     payload = {}
-    # headers = {
-    #   "apikey": 'I0I7g0homYpzaA037a0jvULmQ84U5cjs'
-    # }
     response = requests.request("GET", url, data=payload)
     data = json.loads(response.content)['data']
+    # Возврат округленного до 2ух значения умножения переданного колличества входящей валюты на множитель из запроса
     return round(float(amount) * float(data[f'{D[cur_from]}{D[cur_to]}']), 2)
 
--- bot_functions.py
+++ bot_functions.py
@@ -1,24 +1,38 @@
-from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters, CallbackQueryHandler
+from telegram.ext import CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from converter import *
+from warnings import filterwarnings
+from telegram.warnings import PTBUserWarning
 
-TODO:  'Библиотека python-telegram-bot'
-
-
-def __to_unpack(update, context):  # Распаковка объектов бот и сообщение из полученных аргументов
-    if update.callback_query:
-        return context.application.bot, update.callback_query
-    else:
-        return context.application.bot, update.message
+# Фильтрация предупреждения в ConversationHandler
+filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
 
-converter = ConversationHandler(
+async def help(update, context):  # Команда /help
+    await update.message.reply_text('Бот работает в режиме диалога. После команды /start\n'
+                                    'он начнет диалог и будет предлагать ответы. В конце\n'
+                                    'Вы получите стоимость сконвертированной валюты.\n'
+                                    'Для получения доступных валют, введите /values\n'
+                                    'Для преждевременного выхода из диалога, введите /cancel.')
+
+
+async def values(update, context):  # Команда /values
+    await update.message.reply_text('Список доступных валют:\n'
+                                   '1) Рубль\n2) Доллар\n3) Евро\n4) Юань')
+
+
+async def cancel_err(update, context):  # Отлов команды /cancel вне диалога
+    await update.message.reply_text('Ошибка.\nДиалог не запущен.')
+
+
+converter = ConversationHandler(  # Сборка команды /start
     entry_points=[CommandHandler('start', start_converter)],
     states={
         1: [CallbackQueryHandler(choise_currency)],
         2: [CallbackQueryHandler(get_ammout)],
         3: [MessageHandler(filters.TEXT, converter_result)]
         },
-    fallbacks=[CommandHandler('cancel', cancel)]
+    fallbacks=[CommandHandler('cancel', cancel)],
+    block=False
 )
 
 
